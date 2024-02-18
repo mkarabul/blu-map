1ec0c4923e3e3919cebd677303ea44d32d95fc44
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-02-18 14:51:06 -0500

    Implement auth0 authentication routes

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <UserProvider>
+        <body className={inter.className}>{children}</body>
+      </UserProvider>
     </html>
   );
 }
