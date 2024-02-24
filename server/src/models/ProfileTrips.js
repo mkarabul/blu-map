f09f07565e67efa056a2a6856c14a6196be64fb1
Mert Karabulut <mkarabul@purdue.edu>
2024-02-24 15:59:43 -0500

    Create user profile trip database schema

---
+++ server/src/models/ProfileTrips.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const ProfileTrips = sequelize.define("ProfileTrips", {
+  tripID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  userID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  tripName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  header: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+});
+
+module.exports = ProfileTrips;
