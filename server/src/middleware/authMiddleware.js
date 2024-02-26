6b93fec48c1db1ca7adf73fa63afc5feee4613e2
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-02-26 18:02:01 -0500

    Setup authMiddleware to access userInfo (BUG)

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const { auth } = require("express-oauth2-jwt-bearer");
+const auth0 = require("auth0-js");
 
 const issuerBaseUrl = process.env.AUTH0_ISSUER_BASE_URL;
 const audience = process.env.AUTH0_AUDIENCE || "http://localhost:5000/";
@@ -8,4 +9,22 @@ const checkJwt = auth({
   issuerBaseURL: `${issuerBaseUrl}/`,
 });
 
-module.exports = { checkJwt };
+const auth0Client = new auth0.Authentication({
+  domain: process.env.AUTH0_ISSUER_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+});
+
+const getUserInfoMiddleware = (req, res, next) => {
+  const token = req.auth.token;
+  console.log(token);
+  auth0Client.userInfo(token, (err, userInfo) => {
+    if (err) {
+      return next(err);
+    }
+    
+    req.user = userInfo;
+    next();
+  });
+};
+
+module.exports = { checkJwt, getUserInfoMiddleware };
