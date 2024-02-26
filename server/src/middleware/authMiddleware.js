6e3262963bef3e3ddce04401c03b364398dee767
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-02-26 18:57:42 -0500

    Update to use proper package and request

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const { auth } = require("express-oauth2-jwt-bearer");
-const auth0 = require("auth0-js");
+const { UserInfoClient } = require("auth0");
 
 const issuerBaseUrl = process.env.AUTH0_ISSUER_BASE_URL;
 const audience = process.env.AUTH0_AUDIENCE || "http://localhost:5000/";
@@ -9,22 +9,17 @@ const checkJwt = auth({
   issuerBaseURL: `${issuerBaseUrl}/`,
 });
 
-const auth0Client = new auth0.Authentication({
-  domain: process.env.AUTH0_ISSUER_BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
+const userInfoClient = new UserInfoClient({
+  domain: process.env.AUTH0_DOMAIN,
 });
 
-const getUserInfoMiddleware = (req, res, next) => {
+const getUserInfoMiddleware = async (req, res, next) => {
   const token = req.auth.token;
-  console.log(token);
-  auth0Client.userInfo(token, (err, userInfo) => {
-    if (err) {
-      return next(err);
-    }
-    
-    req.user = userInfo;
-    next();
-  });
+
+  const userInfo = await userInfoClient.getUserInfo(token);
+
+  req.user = userInfo.data;
+  next();
 };
 
 module.exports = { checkJwt, getUserInfoMiddleware };
