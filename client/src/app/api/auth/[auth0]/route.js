5037450f9317040d26de4a350d04c64090767ac4
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-02-29 02:03:22 -0500

    Remove unnecessary comment

--- client/src/app/api/auth/[auth0]/route.js
+++ client/src/app/api/auth/[auth0]/route.js
@@ -1,7 +1,6 @@
 import { handleAuth, handleCallback } from "@auth0/nextjs-auth0";
 
 const afterCallback = async (req, session, state) => {
-  // Add the user's access token to the request headers
   fetch(`${process.env.API_URL}/api/users`, {
     method: "POST",
     headers: {
