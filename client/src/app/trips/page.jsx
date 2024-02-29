f3e6aa603692af09380d059e22d2e05cf51ad12b
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-02-28 20:02:26 -0500

    Redirect user after itinerary creation

--- client/src/app/trips/page.jsx
+++ client/src/app/trips/page.jsx
@@ -2,8 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import Trips from "./components/Trips";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faCircle } from "@fortawesome/free-solid-svg-icons";
+import UseItineraryButton from "./components/NewItineraryButton";
 
 
 const page = () => {
@@ -18,28 +17,13 @@ const page = () => {
   const themeClasses =
     theme === "dark" ? "text-white bg-black" : "text-black bg-white";
 
-  const handleCreateItinerary = async () => {
-    const response = await fetch("/api/itineraries", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({}),
-    });
-    const data = await response.json();
-    console.log(data);
-  };
+
 
   return (
     <div className="container mx-auto px-8 my-8">
       <h1 className="text-center text-4xl font-bold mb-4">Trips</h1>
       <Trips />
-      <button
-        className="btn btn-circle btn-secondary fixed bottom-8 right-8 border-2 align-middle"
-        onClick={handleCreateItinerary}
-      >
-        <FontAwesomeIcon icon={faPlus} className="p-3" size="xl" />        
-      </button>
+      <UseItineraryButton />
     </div>
   );
 };
