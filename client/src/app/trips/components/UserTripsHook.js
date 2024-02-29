a624d0db6277ad47848f24c66bf92ff8e68107d5
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-02-28 20:02:12 -0500

    Remove mock trips

--- client/src/app/trips/components/UserTripsHook.js
+++ client/src/app/trips/components/UserTripsHook.js
@@ -1,16 +1,6 @@
 import { useState, useEffect } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 
-const mockTrips = [
-  { title: "trip", id: "hdfshffjddfjkdfjbkbnnm" },
-  { title: "trip", id: "asdkjfhjdsfj" },
-  { title: "trip", id: "qieoerhfm" },
-  { title: "trip", id: "fadskjgukewqljkl" },
-  { title: "trip", id: "fnmabenr" },
-  { title: "trip", id: "fdsjksdfajhgdsfkhj" },
-  { title: "trip", id: "dafs,jadsfhkfu" },
-];
-
 const useLoadTrips = () => {
   const { user, isLoading: isUserLoading } = useUser();
   const [trips, setTrips] = useState([]);
@@ -22,11 +12,11 @@ const useLoadTrips = () => {
       try {
         const response = await fetch(`api/itineraries/user/${user?.sub}`);
         const data = await response.json();
+        setTrips(data);
       } catch (error) {
         console.error("Error loading trips:", error);
       } finally {
         setIsLoading(false);
-        setTrips(mockTrips);
       }
     };
 
