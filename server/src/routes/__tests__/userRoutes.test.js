c08151e92b279f8e3d1447b855e4468a6bb16502
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-03-01 09:47:01 -0500

    Add test cases for failure check

--- server/src/routes/__tests__/userRoutes.test.js
+++ server/src/routes/__tests__/userRoutes.test.js
@@ -70,3 +70,74 @@ describe("User Routes", () => {
     expect(response.statusCode).toBe(200);
   });
 });
+
+describe("Fail cases User routes", () => {
+  beforeAll(() => {
+    jest.mock("../../middleware/authMiddleware", () => ({
+      checkJwt: jest.fn((req, res, next) => {
+        next();
+      }),
+      getUserInfoMiddleware: jest.fn((req, res, next) => {
+        req.user = undefined;
+        next();
+      }),
+    }));
+  });
+
+  test("GET /users/:userId", async () => {
+    const response = await request(app).get("/notTestUserId");
+    expect(response.statusCode).toBe(404);
+  });
+
+  test("PUT /users/:id", async () => {
+    const response = await request(app)
+      .put("/notTestUserId")
+      .set("Content-Type", "application/json")
+      .send(
+        JSON.stringify({
+          userNameNew: "newTestUserName",
+          genderNew: "newGender",
+          ageNew: 100,
+        })
+      );
+    expect(response.statusCode).toBe(403);
+  });
+
+  test("PUT /users/:id", async () => {
+    const response = await request(app)
+      .put("/testUserId")
+      .set("Content-Type", "application/json")
+      .send(
+        JSON.stringify({
+          userNameNew: "newTestUserName",
+          genderNew: "newGender",
+          ageNew: 100,
+        })
+      );
+    expect(response.statusCode).toBe(404);
+  });
+
+  test("PATCH /users/:userId/toggle-suspend", async () => {
+    const response = await request(app).patch("/notTestUserId/toggle-suspend");
+    expect(response.statusCode).toBe(404);
+  });
+
+  test("PATCH /users/:userId/toggle-admin", async () => {
+    const response = await request(app).patch("/notTestUserId/toggle-admin");
+    expect(response.statusCode).toBe(404);
+  });
+
+  test("PATCH /users/:userId/toggle-darkmode", async () => {
+    const response = await request(app).patch("/notTestUserId/toggle-darkmode");
+    expect(response.statusCode).toBe(404);
+  });
+
+  test("DELETE /users/:userId", async () => {
+    const response = await request(app).delete("/notTestUserId");
+    expect(response.statusCode).toBe(404);
+  });
+
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+});
