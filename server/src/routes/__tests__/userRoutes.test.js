5592c1c0f64214b6d846a3b7bc5d9969d2bb8d61
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-03-01 07:20:29 -0500

    Add unit tests for userRoutes

---
+++ server/src/routes/__tests__/userRoutes.test.js
@@ -0,0 +1,57 @@
+const request = require("supertest");
+const express = require("express");
+const userRoutes = require("../userRoutes");
+
+jest.mock("../../middleware/authMiddleware", () => ({
+  checkJwt: jest.fn((req, res, next) => {
+    next();
+  }),
+  getUserInfoMiddleware: jest.fn((req, res, next) => {
+    req.user = { sub: "testUserId", email: "testUser@example.com" };
+    next();
+  }),
+}));
+
+const app = express();
+app.use(express.json());
+app.use("/", userRoutes);
+
+describe("User Routes", () => {
+  let createdId;
+
+  test("GET /users", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("POST /users", async () => {
+    const response = await request(app).post("/").send();
+    expect(response.statusCode).toBe(201);
+
+    createdId = response.body.id;
+  });
+
+  test("GET /users/:userId", async () => {
+    const response = await request(app).get("/testUserId");
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("PUT /users/:id", async () => {
+    const response = await request(app)
+      .put("/testUserId")
+      .set("Content-Type", "application/json")
+      .send(
+        JSON.stringify({
+          userNameNew: "newTestUserName",
+          genderNew: "newGender",
+          ageNew: 100,
+        })
+      );
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("DELETE /users/:userId", async () => {
+    const response = await request(app).delete("/testUserId");
+    expect(response.statusCode).toBe(200);
+  });
+});
