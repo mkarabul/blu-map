cde4cb99a924875c0d660fe6a15c493d8b05eca1
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-03-01 04:50:41 -0500

    Get data from server API

--- client/src/app/trips/[itineraryId]/edit/page.jsx
+++ client/src/app/trips/[itineraryId]/edit/page.jsx
@@ -2,68 +2,33 @@ import React from "react";
 
 import Form from "./components/Form";
 
+import { getSession } from "@auth0/nextjs-auth0";
+
 const getItinerary = async (itineraryId) => {
-  // Hardcoded data for now
-  const itinerary = {
-    id: 1,
-    title: "Trip to Hawaii",
-    activities: [
-      {
-        id: 1,
-        name: "Snorkeling",
-        start: new Date("2021-01-01T09:00:00"),
-        end: new Date("2021-01-01T12:00:00"),
-      },
-      {
-        id: 2,
-        name: "Beach Day",
-        start: new Date("2021-01-02T13:00:00"),
-        end: new Date("2021-01-02T17:00:00"),
-      },
-      {
-        id: 3,
-        name: "Beach Day",
-        start: new Date("2021-01-02T09:00:00"),
-        end: new Date("2021-01-02T17:00:00"),
-      },
-      {
-        id: 4,
-        name: "Snorkeling",
-        start: new Date("2021-01-01T09:00:00"),
-        end: new Date("2021-01-01T12:00:00"),
-      },
-      {
-        id: 5,
-        name: "Beach Day",
-        start: new Date("2021-01-02T13:00:00"),
-        end: new Date("2021-01-02T17:00:00"),
-      },
-      {
-        id: 6,
-        name: "Beach Day",
-        start: new Date("2021-01-02T09:00:00"),
-        end: new Date("2021-01-02T17:00:00"),
-      },
-      {
-        id: 7,
-        name: "Hiking",
-        start: new Date("2021-01-03T09:00:00"),
-        end: new Date("2021-01-03T12:00:00"),
-      },
-    ],
-  };
+  const user = await getSession();
 
-  return itinerary;
+  const response = await fetch(
+    `${process.env.API_URL}/api/itineraries/${itineraryId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.accessToken}`,
+      },
+    }
+  );
 
-  // const response = await fetch(`api/itinerary/${itineraryId}`);
+  console.log(response);
 
-  // return response.json();
+  return response.json();
 };
 
 const page = async ({ params }) => {
   const { itineraryId } = params;
 
   const itinerary = await getItinerary(itineraryId);
+
+  console.log(itinerary);
 
   return <Form itinerary={itinerary} />;
 };
