85c5d2eeb744590def116a6128270a7a112993be
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-03-01 02:44:18 -0500

    Implement post link

---
+++ client/src/app/post/[postId]/page.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { getSession } from "@auth0/nextjs-auth0";
+
+import SocialPost from "../../social/components/social-post";
+
+const getPost = async (postId) => {
+  const user = await getSession();
+
+  const response = await fetch(
+    `${process.env.API_URL}/api/profile-trip/${postId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.accessToken}`,
+      },
+    }
+  );
+
+  const post = await response.json();
+  return post;
+};
+
+const Post = async ({ params }) => {
+  const { postId } = params;
+  const post = await getPost(postId);
+
+  if (!post) {
+    return;
+  }
+
+  return <div className="container mx-auto">
+    <SocialPost header={post.header} description={post.description} tripDate={post.tripDate} userName={post.userName} />
+  </div>;
+};
+
+export default Post;
