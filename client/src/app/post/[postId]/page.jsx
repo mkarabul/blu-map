2114a6d0b6c50c729b3acf319e2c9c477a0ade1e
Meet Patel <pate1724@purdue.edu>
2024-03-01 10:51:21 -0500

    update page.js to remove redundant code

--- client/src/app/post/[postId]/page.jsx
+++ client/src/app/post/[postId]/page.jsx
@@ -1,22 +1,4 @@
 import React from "react";
-
-import SocialPost from "../../social/components/social-post";
-
-const getPost = async (postId) => {
-  const response = await fetch(`/api/post/${postId}`);
-  const post = await response.json();
-  return post;
-};
-
-const Post = async ({ params }) => {
-  const { postId } = params;
-  const post = await getPost(postId);
-  return <div>Post</div>;
-};
-
-export default Post;
-
-import React from "react";
 import { getSession } from "@auth0/nextjs-auth0";
 
 import SocialPost from "../../social/components/social-post";
