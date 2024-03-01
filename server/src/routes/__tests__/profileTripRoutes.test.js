755cfd3859ab048a0fa455c2df01fe6b1566eb19
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-03-01 09:54:00 -0500

    Add profileTripRoutes failure cases

--- server/src/routes/__tests__/profileTripRoutes.test.js
+++ server/src/routes/__tests__/profileTripRoutes.test.js
@@ -52,3 +52,25 @@ describe("Profile Trip Routes", () => {
     expect(response.statusCode).toBe(200);
   });
 });
+
+describe("Fail Cases Profile Trip Routes", () => {
+  test("GET /profile-trips/:id", async () => {
+    const response = await request(app).get("/123");
+    expect(response.statusCode).toBe(500);
+  });
+
+  test("GET /profile-trips/user/:userId", async () => {
+    const response = await request(app).get("/user/123");
+    expect(response.statusCode).toBe(403);
+  });
+
+  test("POST /profile-trips", async () => {
+    const response = await request(app).post("/");
+    expect(response.statusCode).toBe(500);
+  });
+
+  test("GET /profile-trips/social", async () => {
+    const response = await request(app).get("/social");
+    expect(response.statusCode).toBe(500);
+  });
+});
