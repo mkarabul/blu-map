0889d657ca28dfb2f3965c514db94db70f119ef9
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-03-10 19:02:51 -0400

    Update to export client directly

--- server/src/config/cache.js
+++ server/src/config/cache.js
@@ -6,40 +6,8 @@ client.on("error", (error) => {
   console.error("Error connecting to Redis:", error);
 });
 
-const set = (key, value) => {
-  return new Promise((resolve, reject) => {
-    client.set(key, value, (error, reply) => {
-      if (error) {
-        console.error("Error setting value in Redis:", error);
-        reject(error);
-      } else {
-        console.log("Value set in Redis:", reply);
-        resolve(reply);
-      }
-    });
-  });
-};
+client.connect().then(() => {
+  console.log("Connected to Redis");
+});
 
-const get = (key) => {
-  return new Promise((resolve, reject) => {
-    client.get(key, (error, reply) => {
-      if (error) {
-        console.error("Error getting value from Redis:", error);
-        reject(error);
-      } else {
-        console.log("Value from Redis:", reply);
-        resolve(reply);
-      }
-    });
-  });
-};
-
-const quit = () => {
-  client.quit();
-};
-
-module.exports = {
-  set,
-  get,
-  quit,
-};
+module.exports = client;
