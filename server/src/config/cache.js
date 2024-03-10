64e118428274e8770188b3a69bda3b1f5d984a53
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-03-10 19:05:26 -0400

    Add custom URL for redis cache

--- server/src/config/cache.js
+++ server/src/config/cache.js
@@ -1,6 +1,8 @@
 const { createClient } = require("redis");
 
-const client = createClient();
+const client = createClient({
+    url: process.env.REDIS_URL,
+});
 
 client.on("error", (error) => {
   console.error("Error connecting to Redis:", error);
