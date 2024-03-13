e2cd4b7408aa0c0587e4a34a428c2d6228aef49d
Meet Patel <pate1724@purdue.edu>
2024-03-13 17:20:15 -0400

    allow the likes/dislikes to be saved after a refresh and follow traditional click patterns on like/dislike

--- client/src/app/social/components/rep_system.jsx
+++ client/src/app/social/components/rep_system.jsx
@@ -3,17 +3,61 @@ import { useState } from "react";
 export const RepSystem = (initialLikes, initialDislikes, postId) => {
   const [likes, setLikes] = useState(initialLikes);
   const [dislikes, setDislikes] = useState(initialDislikes);
+  const [hasLiked, setHasLiked] = useState(false);
+  const [hasDisliked, setHasDisliked] = useState(false);
+  const [firstLike, setFirstLike] = useState(true);
+  const [firstDislike, setFirstDislike] = useState(true);
 
   const addLike = () => {
-    setLikes(likes + 1);
-    fetch(`/api/profile-trip/${postId}/increment-likes`, { method: "PATCH" });
+    if (!hasLiked) {
+      setLikes((likes) => likes + 1);
+      setHasLiked(true);
+      if (firstLike) {
+        fetch(`/api/profile-trip/${postId}/increment-likes`, {
+          method: "PATCH",
+        });
+      }
+      setFirstLike(false);
+
+      if (hasDisliked) {
+        setDislikes((dislikes) => dislikes - 1);
+        setHasDisliked(false);
+        fetch(`/api/profile-trip/${postId}/decrement-dislikes`, {
+          method: "PATCH",
+        });
+      }
+    } else {
+      setLikes((likes) => likes - 1);
+      setHasLiked(false);
+      fetch(`/api/profile-trip/${postId}/decrement-likes`, { method: "PATCH" });
+    }
   };
 
   const addDislike = () => {
-    setDislikes(dislikes + 1);
-    fetch(`/api/profile-trip/${postId}/increment-dislikes`, {
-      method: "PATCH",
-    });
+    if (!hasDisliked) {
+      setDislikes((dislikes) => dislikes + 1);
+      setHasDisliked(true);
+      if (firstDislike) {
+        fetch(`/api/profile-trip/${postId}/increment-dislikes`, {
+          method: "PATCH",
+        });
+      }
+      setFirstDislike(false);
+
+      if (hasLiked) {
+        setLikes((likes) => likes - 1);
+        setHasLiked(false);
+        fetch(`/api/profile-trip/${postId}/decrement-likes`, {
+          method: "PATCH",
+        });
+      }
+    } else {
+      setDislikes((dislikes) => dislikes - 1);
+      setHasDisliked(false);
+      fetch(`/api/profile-trip/${postId}/decrement-dislikes`, {
+        method: "PATCH",
+      });
+    }
   };
 
   return { likes, dislikes, addLike, addDislike };
