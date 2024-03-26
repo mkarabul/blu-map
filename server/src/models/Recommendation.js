d49f7e3404fccecf17f78c83c895ff1ea34aa2a4
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-03-26 19:09:12 -0400

    Create server routes for user latest recommendations

---
+++ server/src/models/Recommendation.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Recommendation = sequelize.define("Recommendation", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  userId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  activity: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+Recommendation.sync({ alter: true });
+
+module.exports = Recommendation;
--- server/src/routes/apiRouter.js
+++ server/src/routes/apiRouter.js
@@ -6,6 +6,8 @@ const itineraryRoutes = require("./itineraryRoutes");
 const adminRoutes = require("./adminRoutes");
 const socialPostRoutes = require("./socialPostRoutes");
 const notificationRoutes = require("./notificationRoutes");
+const recommendationRoutes = require("./recommendationRoutes");
+
 const router = express.Router();
 
 router.use("/users", userRoutes);
@@ -14,5 +16,6 @@ router.use("/profile-trip", profileTripRoutes);
 router.use("/itineraries", itineraryRoutes);
 router.use("/social-post", socialPostRoutes);
 router.use("/notification", notificationRoutes);
+router.use("/recommendations", recommendationRoutes);
 
 module.exports = router;
