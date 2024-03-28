92b99d0f33092dbce4bf122e601b477d5f999b5d
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-03-27 21:20:44 -0400

    Add UUID for differentiation

--- server/src/models/Comment.js
+++ server/src/models/Comment.js
@@ -3,19 +3,26 @@ const sequelize = require("../config/database");
 const { use } = require("../routes/profileTripRoutes");
 
 const Comment = sequelize.define("Comment", {
+  uuid: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+
   userId: {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  postId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
   userName: {
     type: DataTypes.STRING,
     allowNull: false,
   },
   comment: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  postId: {
     type: DataTypes.STRING,
     allowNull: false,
   },
