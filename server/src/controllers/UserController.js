c71bb03ff1827d953061873a77f438f439bbb87a
stanleytk <kim4111@purdue.edu>
2024-03-28 23:49:21 -0400

    Updated User Controller

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const ProfileTrip = require("../models/ProfileTrip");
 const {
   generateFromEmail,
   generateUsername,
@@ -259,26 +260,36 @@ const UserController = {
     } catch (error) {
       res.status(500).json({ error: "Internal Server Error" });
     }
-  },
-  async updateUserModeByUserId(req, res) {
+  },async updateUserModeByUserId(req, res) {
     try {
       const { userId } = req.params;
       const { isPublic } = req.body;
-      // if (!isPublic) {
-      //   return res.status(500).json({ error: "Internal Server Error" });
-      // }
+  
+  
       const user = await User.findOne({ where: { userId } });
       if (!user) {
         return res.status(404).json({ error: "User not found" });
       }
       user.isPublic = isPublic;
       await user.save();
+  
+      const profiles = await ProfileTrip.findAll({ where: { userId } });
+      if (profiles.length === 0) {
+        return res.status(404).json({ error: "Profile not found" });
+      }
+  
+      await Promise.all(profiles.map(async (profile) => {
+        profile.isPublic = isPublic;
+        await profile.save();
+      }));
+  
       res.status(200).json({ message: "User mode updated successfully" });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Internal Server Error" });
     }
   },
+  
   
 };
 
