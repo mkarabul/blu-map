089a096c20de6bf45ac2b0722f9b6bb1abbcf94e
Mert Karabulut <mkarabul@purdue.edu>
2024-03-29 07:56:04 -0400

    Add profileTrip tests

--- client/src/app/profile/components/FriendButton.jsx
+++ client/src/app/profile/components/FriendButton.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { useState, useEffect } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 
--- server/src/controllers/ProfileTripController.js
+++ server/src/controllers/ProfileTripController.js
@@ -194,6 +194,7 @@ const ProfileTripsController = {
       profileTrip.increment("likes");
       res.status(200).json(profileTrip);
     } catch (error) {
+      console.error(error);
       res.status(500).json({ error: "Internal Server Error" });
     }
   },
