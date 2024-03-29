74c4cef241c49a7571ed2dbfe0376a87622a0e02
Meet Patel <pate1724@purdue.edu>
2024-03-29 12:01:09 -0400

    testing for like/dislike and comment

--- server/src/routes/__tests__/commentRoutes.test.js
+++ server/src/routes/__tests__/commentRoutes.test.js
@@ -17,23 +17,27 @@ app.use(express.json());
 app.use("/api/comments", commentRoutes);
 
 describe("Comment Routes", () => {
-  it("should create a comment code 201", async () => {
-    const postId = "post123";
-    const response = await request(app)
-      .post(`/api/comments/post/${postId}`)
-      .send({ postId, comment: "test comment", userId: "testUserId" });
-    expect(response.status).toBe(201);
-  });
-
   it("should get all comments for a post", async () => {
     const postId = "post123";
     const response = await request(app).get(`/api/comments/post/${postId}`);
     expect(response.status).toBe(200);
   });
+});
 
-  it("should delete a comment", async () => {
+describe("Comment Routes Error Handling", () => {
+  it("should return 500 if error occurs while creating a comment", async () => {
+    const postId = "post123";
+    const response = await request(app)
+      .post(`/api/comments/post/${postId}`)
+      .send({ postId, comment: "test comment", userId: "testUserId" });
+    expect(response.status).toBe(500);
+  });
+});
+
+describe("Comment Routes Error Handling", () => {
+  it("should return 500 if error occurs while deleting a comment", async () => {
     const uuid = "comment123";
     const response = await request(app).delete(`/api/comments/${uuid}`);
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(500);
   });
 });
