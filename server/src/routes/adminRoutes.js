6fc120d4552e18d0059afe0a202dc3c36b86a199
stanleytk <kim4111@purdue.edu>
2024-04-07 13:19:49 -0400

    fixed bugs in the server

--- server/src/routes/adminRoutes.js
+++ server/src/routes/adminRoutes.js
@@ -15,9 +15,9 @@ router.patch(
 router.patch("/:userId/toggle-admin", UserController.toggleUserAdminStatusById);
 
 
-router.post("/notifications", UserController.postAdminNotification);
-router.get("/notifications/:userId", UserController.getNotificationsByUserId);
-router.get("/notifications", UserController.getAllNotifications); 
-router.delete("/notifications/:notificationId", UserController.deleteNotificationById);
+router.post("/notifications/post", UserController.postAdminNotification);
+router.get("/notifications/get", UserController.getAllNotifications); 
+router.get("/notifications/get/:userId", UserController.getNotificationsByUserId);
+router.delete("/notifications/delete/:notificationId", UserController.deleteNotificationById);
 
 module.exports = router;
