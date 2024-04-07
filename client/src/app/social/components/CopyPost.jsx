6305beb9eaea5dfa2e525c08a8942aa19ba51ee6
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-04-07 17:07:04 -0400

    Use routes to copy post itinerary and redirect

--- client/src/app/social/components/CopyPost.jsx
+++ client/src/app/social/components/CopyPost.jsx
@@ -1,16 +1,35 @@
+"use client";
+
 import React from "react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const CopyPost = ({ tripId }) => {
+  const router = useRouter();
+
+  const copy = async () => {
+    const response = await fetch(`/api/itineraries/${tripId}/copy`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      console.error("Failed to copy post");
+      return;
+    }
+
+    const data = await response.json();
+
+    router.push(`/trips/${data.uuid}`);
+  };
+
   return (
-    <Link href={`/trips/${tripId}`}>
-      <button className="btn btn-outline rounded-full">
-        <FontAwesomeIcon icon={faPlus} />
-      </button>
-    </Link>
+    <button className="btn btn-outline rounded-full" onClick={copy}>
+      <FontAwesomeIcon icon={faPlus} />
+    </button>
   );
 };
 
