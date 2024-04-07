174af5e52113f2a5129e623b7e976ab3b2acc585
stanleytk <kim4111@purdue.edu>
2024-04-07 12:45:04 -0400

    Created backend functionalities for admin notification

--- server/src/routes/adminRoutes.js
+++ server/src/routes/adminRoutes.js
@@ -14,4 +14,10 @@ router.patch(
 );
 router.patch("/:userId/toggle-admin", UserController.toggleUserAdminStatusById);
 
+
+router.post("/notifications", UserController.postAdminNotification);
+router.get("/notifications/:userId", UserController.getNotificationsByUserId);
+router.get("/notifications", UserController.getAllNotifications); 
+router.delete("/notifications/:notificationId", UserController.deleteNotificationById);
+
 module.exports = router;
