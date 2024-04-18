dd658a54e2de2352dba5ed7eaf52a4ef84b925b4
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-04-18 19:35:52 -0400

    Update with proper API call

--- server/src/config/googleMapsClient.js
+++ server/src/config/googleMapsClient.js
@@ -1,5 +1,5 @@
 const { Client } = require("@googlemaps/google-maps-services-js");
 
-const client = new Client({});
+const client = new Client();
 
 module.exports = client;
--- server/src/controllers/PlacesController.js
+++ server/src/controllers/PlacesController.js
@@ -1,18 +1,22 @@
-import { request } from "express";
-import client from "../config/googleMapsClient";
+const { GetPublicAccessBlockCommand } = require("@aws-sdk/client-s3");
+const client = require("../config/googleMapsClient");
 
 const PlacesController = {
-  requestNearbyPlaces: async (req, res) => {
+  getPlaces: async (req, res) => {
     try {
       const { location } = req.query;
       const request = {
         params: {
           location,
+          type: "tourist_attraction",
+          radius: 5000,
+          rankby: "prominence",
+          key: process.env.GOOGLE_MAPS_API_KEY,
         },
       };
 
       const response = await client.placesNearby(request);
-      res.status(200).json(response.data);
+      res.status(200).json(response.data.results);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Internal Server Error" });
