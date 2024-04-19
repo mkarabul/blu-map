f1fb59fa22b2fb25528086c9b2bcb288a55d7908
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-04-19 16:29:27 -0400

    Remove bug: Add button

--- client/src/app/trips/[itineraryId]/edit/components/AddActivityButton.jsx
+++ client/src/app/trips/[itineraryId]/edit/components/AddActivityButton.jsx
@@ -2,21 +2,20 @@ import React from "react";
 
 const AddActivityButton = ({ addActivity, defaultStart, defaultEnd }) => {
   return (
-    // <button
-    //   type="button"
-    //   className="btn btn-primary"
-    //   onClick={() =>
-    //     addActivity({
-    //       name: "New Activity",
-    //       start: defaultStart,
-    //       end: defaultEnd,
-    //       id: crypto.randomUUID(),
-    //     })
-    //   }
-    // >
-    //   Add Activity
-    // </button>
-    <></>
+    <button
+      type="button"
+      className="btn btn-primary"
+      onClick={() =>
+        addActivity({
+          name: "New Activity",
+          start: defaultStart,
+          end: defaultEnd,
+          id: crypto.randomUUID(),
+        })
+      }
+    >
+      Add Activity
+    </button>
   );
 };
 
